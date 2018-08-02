d8dffba70c05271f4e378732977940f113d2257e
Simon Joyce <sj.malice+git@gmail.com>
2018-08-02 10:31:00 -0400

    First. Basic functional spider achieved.

---
+++ carsDotComReviews/items.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+
+class carsDotComReviewsItem(Item):
+    # define the fields for your item here like:
+    # name = Field()
+    #make = Field()   #add these later after correct way to loop through urls is done
+    #model = Field()
+    #modelYear = Field()
+    rating = Field()
+    url = Field()
+    title = Field()
+    author = Field()
+    location = Field()
+    #city = Field()
+    #state = Field()
+    date = Field()
+    reviewBody = Field()
+    comfort = Field()
+    exteriorStyling = Field()
+    value = Field()
+    performance = Field()
+    interior = Field()
+    reliability = Field()
+    new = Field()
+    use = Field()
+    recommend = Field()
+    helpful = Field()
+    outOf = Field()
---
+++ carsDotComReviews/spiders/carsDotComSpider.py
@@ -0,0 +1,60 @@
+from scrapy import Spider
+from carsDotComReviews.items import carsDotComReviewsItem
+
+class carsDotComSpider(Spider):
+    name = 'cdcspider'
+    allowed_urls = ['https://www.cars.com/research/']
+    start_urls = ['https://www.cars.com/research/honda-accord-2017/consumer-reviews/?pg=1']
+    
+    def parse(self, response):
+        reviews = response.xpath('//article') #tag for the user reviews
+        print(len(reviews))
+        print("="*40)
+        i = 1
+        for review in reviews:
+            print(i)
+            print("="*40)
+            i += 1
+            rating = review.xpath('./cars-star-rating/@rating').extract_first()
+            url = review.xpath('./p[1]/a/@href').extract_first()
+            title = review.xpath('./p[1]/a/text()').extract_first()
+            reviewerInfo = review.xpath('./p[2]/text()').extract_first().split("\n")[1:4] #contains username, location and time
+            #location = reviewerInfo[1].lstrip().split(", ")
+            reviewBody = review.xpath('./p[3]/text()').extract_first().strip().replace('\n', ' ')
+            comfort = review.xpath('./div/div[1]/cars-star-rating/@rating').extract_first()
+            performance = review.xpath('./div/div[2]/cars-star-rating/@rating').extract_first()
+            exteriorStyling = review.xpath('./div/div[3]/cars-star-rating/@rating').extract_first()
+            interior = review.xpath('./div/div[4]/cars-star-rating/@rating').extract_first()
+            value = review.xpath('./div/div[5]/cars-star-rating/@rating').extract_first()
+            reliability = review.xpath('./div/div[6]/cars-star-rating/@rating').extract_first()
+            new = review.xpath('./p[4]/b/text()').extract_first()
+            use = review.xpath('./p[5]/b/text()').extract_first()
+            recommend = review.xpath('./p[6]/b/text()').extract_first()
+            helpful = review.xpath('./p[7]/b[1]/text()').extract_first()
+            outOf = review.xpath('./p[7]/b[2]/text()').extract_first()
+            
+            item = carsDotComReviewsItem()
+            #item['make'] = make  #add these later after correct way to loop through urls is
+            #item['model'] = model
+            #item['modelYear'] = modelYear
+            item['rating'] = rating
+            item['url'] = url
+            item['title'] = title
+            item['author'] = reviewerInfo[0].lstrip()[3:]
+            item['location'] = reviewerInfo[1].lstrip()[5:]
+            #item['city'] = location[0][5:]
+            #item['state'] = location[1]
+            item['date'] = reviewerInfo[2].lstrip()[3:]
+            item['reviewBody'] = reviewBody
+            item['comfort'] = comfort
+            item['exteriorStyling'] = exteriorStyling
+            item['value'] = value
+            item['performance'] = performance
+            item['interior'] = interior
+            item['reliability'] = reliability
+            item['new'] = new
+            item['use'] = use
+            item['recommend'] = recommend
+            item['helpful'] = helpful
+            item['outOf'] = outOf
+            yield item
